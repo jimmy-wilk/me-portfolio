c708b811621d5d25c3c735681187e5733f1d1b0d
jimmy wilk <mafsjimmy@gmail.com>
2024-05-21 16:34:38 +0530

    what is going on

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
-import Projects from './sections/Projects/Projects';
+// import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
     <>
       <Hero />
       <Skills />
+      
       <Contact />
       <Footer />
     </>
